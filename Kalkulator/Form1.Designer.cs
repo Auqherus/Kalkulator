f5dab89420b3a3b61cf4f9979aa5c3b40058ccac
Auqherus <patrykromanowski@gmail.com>
2022-04-28 10:05:05 +0200

    Dodaj pliki projektów.

---
+++ Kalkulator/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace Kalkulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wyswietlacz = new System.Windows.Forms.TextBox();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.bequal = new System.Windows.Forms.Button();
+            this.brazy = new System.Windows.Forms.Button();
+            this.bminus = new System.Windows.Forms.Button();
+            this.bplus = new System.Windows.Forms.Button();
+            this.bdivine = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // wyswietlacz
+            // 
+            this.wyswietlacz.Location = new System.Drawing.Point(48, 22);
+            this.wyswietlacz.Name = "wyswietlacz";
+            this.wyswietlacz.Size = new System.Drawing.Size(138, 23);
+            this.wyswietlacz.TabIndex = 0;
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(48, 51);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(30, 27);
+            this.b7.TabIndex = 1;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(84, 51);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(30, 27);
+            this.b8.TabIndex = 2;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(120, 51);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(30, 27);
+            this.b9.TabIndex = 3;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(120, 84);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(30, 27);
+            this.b6.TabIndex = 6;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(84, 84);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(30, 27);
+            this.b5.TabIndex = 5;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(48, 84);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(30, 27);
+            this.b4.TabIndex = 4;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(120, 117);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(30, 27);
+            this.b3.TabIndex = 9;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(84, 117);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(30, 27);
+            this.b2.TabIndex = 8;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(48, 117);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(30, 27);
+            this.b1.TabIndex = 7;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b0
+            // 
+            this.b0.Location = new System.Drawing.Point(48, 150);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(30, 27);
+            this.b0.TabIndex = 10;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.Click += new System.EventHandler(this.b0_Click);
+            // 
+            // bequal
+            // 
+            this.bequal.Location = new System.Drawing.Point(84, 150);
+            this.bequal.Name = "bequal";
+            this.bequal.Size = new System.Drawing.Size(66, 27);
+            this.bequal.TabIndex = 11;
+            this.bequal.Text = "=";
+            this.bequal.UseVisualStyleBackColor = true;
+            this.bequal.Click += new System.EventHandler(this.bequal_Click);
+            // 
+            // brazy
+            // 
+            this.brazy.Location = new System.Drawing.Point(156, 117);
+            this.brazy.Name = "brazy";
+            this.brazy.Size = new System.Drawing.Size(30, 27);
+            this.brazy.TabIndex = 14;
+            this.brazy.Text = "*";
+            this.brazy.UseVisualStyleBackColor = true;
+            this.brazy.Click += new System.EventHandler(this.brazy_Click);
+            // 
+            // bminus
+            // 
+            this.bminus.Location = new System.Drawing.Point(156, 84);
+            this.bminus.Name = "bminus";
+            this.bminus.Size = new System.Drawing.Size(30, 27);
+            this.bminus.TabIndex = 13;
+            this.bminus.Text = "-";
+            this.bminus.UseVisualStyleBackColor = true;
+            this.bminus.Click += new System.EventHandler(this.bminus_Click);
+            // 
+            // bplus
+            // 
+            this.bplus.Location = new System.Drawing.Point(156, 51);
+            this.bplus.Name = "bplus";
+            this.bplus.Size = new System.Drawing.Size(30, 27);
+            this.bplus.TabIndex = 12;
+            this.bplus.Text = "+";
+            this.bplus.UseVisualStyleBackColor = true;
+            this.bplus.Click += new System.EventHandler(this.bplus_Click);
+            // 
+            // bdivine
+            // 
+            this.bdivine.Location = new System.Drawing.Point(156, 150);
+            this.bdivine.Name = "bdivine";
+            this.bdivine.Size = new System.Drawing.Size(30, 27);
+            this.bdivine.TabIndex = 15;
+            this.bdivine.Text = "/";
+            this.bdivine.UseVisualStyleBackColor = true;
+            this.bdivine.Click += new System.EventHandler(this.bdivine_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Kalkulator.Properties.Resources.stars;
+            this.ClientSize = new System.Drawing.Size(245, 184);
+            this.Controls.Add(this.bdivine);
+            this.Controls.Add(this.brazy);
+            this.Controls.Add(this.bminus);
+            this.Controls.Add(this.bplus);
+            this.Controls.Add(this.bequal);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.wyswietlacz);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kalkulatorek Patryk Romanowski";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox wyswietlacz;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button bequal;
+        private System.Windows.Forms.Button brazy;
+        private System.Windows.Forms.Button bminus;
+        private System.Windows.Forms.Button bplus;
+        private System.Windows.Forms.Button bdivine;
+    }
+}
+
---
+++ Kalkulator/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        string FirstNumber, SecondNumber;
+        char WhatToDo = ' ';
+        
+
+        private void bplus_Click(object sender, EventArgs e)
+        {
+            WhatToDo = '+';
+            wyswietlacz.Text = "";
+        }
+
+        private void bminus_Click(object sender, EventArgs e)
+        {
+            WhatToDo = '-';
+            wyswietlacz.Text = "";
+        }
+
+        private void brazy_Click(object sender, EventArgs e)
+        {
+            WhatToDo = '*';
+            wyswietlacz.Text = "";
+        }
+
+        private void bdivine_Click(object sender, EventArgs e)
+        {
+            WhatToDo = '/';
+            wyswietlacz.Text = "";
+        }
+
+        private void bequal_Click(object sender, EventArgs e)
+        {
+            switch (WhatToDo)
+            {
+                
+                case ('+'):
+                   
+                    wyswietlacz.Text = (int.Parse(FirstNumber) + int.Parse(SecondNumber)).ToString();
+                    break;
+                case ('-'):
+                    wyswietlacz.Text = (int.Parse(FirstNumber) - int.Parse(SecondNumber)).ToString();
+                    break;
+                case ('*'):
+                    wyswietlacz.Text = (int.Parse(FirstNumber) * int.Parse(SecondNumber)).ToString();
+                    break;
+                case ('/'):
+                    if (int.Parse(SecondNumber) == 0)
+                    {
+                        wyswietlacz.Text = "Nie dziel przez zero!";
+                    }
+                    else
+                    {
+                        wyswietlacz.Text = (int.Parse(FirstNumber) / int.Parse(SecondNumber)).ToString();
+                    }
+                    break;
+
+                
+
+            }
+
+            FirstNumber = "";
+            SecondNumber = "";
+            WhatToDo = ' ';
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            Calculation(1);
+        }
+        private void b2_Click(object sender, EventArgs e)
+        {
+            Calculation(2);
+        }
+        private void b3_Click(object sender, EventArgs e)
+        {
+            Calculation(3);
+        }
+        private void b4_Click(object sender, EventArgs e)
+        {
+            Calculation(4);
+        }
+        private void b5_Click(object sender, EventArgs e)
+        {
+            Calculation(5);
+        }
+        private void b6_Click(object sender, EventArgs e)
+        {
+            Calculation(6);
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            Calculation(7);
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            Calculation(8);
+        }
+        private void b9_Click(object sender, EventArgs e)
+        {
+            Calculation(9);
+        }
+
+        private void b0_Click(object sender, EventArgs e)
+        {
+            Calculation(0);
+        }
+
+        private void Calculation(int number)
+        {
+
+
+
+
+            if (WhatToDo.Equals(' '))
+            {
+
+                FirstNumber += number;
+                wyswietlacz.Text = FirstNumber;
+            }
+            else 
+                {
+                    SecondNumber += number;
+                    wyswietlacz.Text = SecondNumber;
+                }
+
+            
+        }
+
+      
+
+    }
+}
